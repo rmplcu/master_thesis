e9e57f380180aa7b40d6f591f4566d33fea26f71
Luca Rompani <72087404+rmplcu@users.noreply.github.com>
2024-03-17 16:27:49 +0100

    Delete workspace/build directory

--- workspace/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/luca/Desktop/master_thesis/workspace/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/luca/Desktop/master_thesis/workspace/devel;/home/luca/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/luca/Desktop/master_thesis/workspace/devel;/home/luca/catkin_ws/devel;/opt/ros/noetic' != '' else []
